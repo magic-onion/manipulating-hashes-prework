1f6a0506f20207357b55a2192e5f83c6c0b46b7d
Alexander Truong <alexander.m.truong@gmail.com>
2018-09-30 14:10:45 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,8 +14,13 @@ def first_challenge
   }
 
   #your code here
-
-
+contacts.each do |person, data|
+  person.each do |attribute, value|
+    if value == "strawberry"
+      value.delete
+    end
+  end
+end
   #remember to return your newly altered contacts hash!
   contacts
 end
